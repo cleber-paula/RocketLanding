faea2d8f7dd1b54c38aa3ddf82f80af876860f66
cleber-paula <cleber.pa.sp@gmail.com>
2022-08-20 01:14:33 +0100

    Fix

--- IArea.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RocketLanding
-{
-    public interface IArea
-    {
-        string CheckLanding(int rocketID, int row, int column);
-    }
-}
--- RocketLanding.Tests/RocketLandingTests.cs
+++ RocketLanding.Tests/RocketLandingTests.cs
@@ -141,3 +141,25 @@ public class RocketLandingTests
 
 }
 
+[TestFixture]
+public class MyTests
+{
+    [TestCaseSource(typeof(MyDataClass), nameof(MyDataClass.TestCases))]
+    public int DivideTest(int n, int d)
+    {
+        return n / d;
+    }
+}
+
+public class MyDataClass
+{
+    public static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(12, 3).Returns(4);
+            yield return new TestCaseData(12, 2).Returns(6);
+            yield return new TestCaseData(12, 4).Returns(3);
+        }
+    }
+}
